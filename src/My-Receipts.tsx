a62717256359b47ccd75c1396792499961216d3a
ColtonBothe-IT <coltonbothe@gmail.com>
2022-04-16 06:36:02 -0500

    created and edited new files.
    created temporary logo and started working on the components for the site.

--- src/My-Receipts.tsx
+++ src/My-Receipts.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import Nav from './Nav';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      yay
     </div>
   );
 }
---
+++ src/Nav.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function Nav() {
+  return (
+    <nav>
+      <h3>Logo</h3>
+      <ul>
+        <li></li>
+        <li></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
