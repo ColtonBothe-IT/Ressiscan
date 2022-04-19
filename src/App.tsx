a7e202b11579cf47649017eaca1de4a997dde644
ColtonBothe-IT <coltonbothe@gmail.com>
2022-04-19 07:22:18 -0500

    Created SVG, Added logo to site.
    updated manifest.json to properly display the correct information. Added some temp styles to app.css and index.css.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import {
-  BrowserRouter as Router, Route
+  BrowserRouter as Router, Route, Link
 } from "react-router-dom";
 import './App.css';
 import Nav from './Nav';
 
+import logo from './ressiscan-logo.svg'
+
 function App() {
   return (
+    
     <div className="App">
-      
+      <img src={logo} alt="logo" height={200} width={150}/>
+      <br />
+      Hi Jesus
     </div>
   );
 }
