98916d8285f72a371661b92f895c3684fcf324ab
ColtonBothe-IT <coltonbothe@gmail.com>
2022-05-11 03:04:22 -0500

    Added Raleway font
    added font via googlefonts link in index.html.
    
    created button files to create buttons for home page.

--- src/home.tsx
+++ src/home.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import './home.css';
 
 
 function Home() {
     return (
-        <h1>Home</h1>
+        <>
+        <h1 className="heading">Ressiscan</h1>
+        <div className="heading-desc">The place to store all of your receipts!</div>
+        </>
     );
 }
 
