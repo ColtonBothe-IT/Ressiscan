35412d5908fd1e3610e144bb1fd6c82dd77f0a8c
ColtonBothe-IT <coltonbothe@gmail.com>
2022-06-09 01:20:13 -0500

    Update PopupComponent.tsx
    planning to design popup component to show camera and ask for perms before doing so. will have close button and button for camera action. added TODO at end of file..

--- src/components/PopupComponent.tsx
+++ src/components/PopupComponent.tsx
@@ -2,17 +2,33 @@ import React from 'react';
 import '../App.css';
 
 interface Props {
-
+    border: string;
 }
 
 const Popup: React.FC<Props> = ({
-    
+    border
 }) => {
     return (
-        <div>
-
+        <div className='popup-container' style={{
+            border
+        }}>
+            <div></div>
         </div>
+        
     )
 }
 
 export default Popup;
+
+/*
+    to do: 
+    make X close button make camera window.
+    make camera ask for perms. 
+    make camera button. 
+
+    after photo is taken will show small prev.
+    of photo.
+    can confirm or retake.
+    if confirmed save to mongodb database 
+    after OCR hav converted to pdf.
+ */
