57e91fac65e6552f4cfed02a61a7eec5c3f5ec67
ColtonBothe-IT <coltonbothe@gmail.com>
2022-06-08 19:32:47 -0500

    Create PopupComponent.tsx

---
+++ src/components/PopupComponent.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../App.css';
+
+interface Props {
+
+}
+
+const Popup: React.FC<Props> = ({
+    
+}) => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Popup;
