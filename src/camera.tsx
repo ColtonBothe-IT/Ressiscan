68c277174531acbe17598b735e21220ceb3601ca
ColtonBothe-IT <coltonbothe@gmail.com>
2022-06-16 02:04:51 -0500

    Create actual camera container and add camera functionalities.

--- src/camera.tsx
+++ src/camera.tsx
@@ -1,17 +1,54 @@
-import React from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import './App.css';
+import './Camera.css'
 
 
 function Camera() {
+
+    const videoRef = useRef(null);
+    const photoRef = useRef(null);
+
+    const [hasPhoto, setHasPhoto] = useState(false);
+
+    const getVideo = () => {
+        navigator.mediaDevices.getUserMedia({ video: { width: 253.125, height: 450 } })
+        .then(stream => {
+            let video = videoRef.current;
+            video.srcObject = stream;
+            video.play();
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    }
+
+    useEffect(() => {
+        getVideo();
+    }, [videoRef])
+
+    function Print() {
+        console.log("yay!");
+    }
+
     return (
         <>
+
         <h1>Camera</h1>
         <div className='camera-container'>
-            <div className='camera'>
-                Camera Box
+            <div className='camera-box'>
+                <video ref={ videoRef }></video>
             </div>
-            <button className='camera-button'>o</button>
-        </div></>
+            <button className='camera-button' onClick={Print}>o</button>
+            <div className={"result" + (hasPhoto ? 'hasPhoto' : '')}>
+                {/* <canvas ref={ photoRef }></canvas>
+                <button>CLOSE</button> */}
+            </div>
+        </div>
+
+        <div className='lowercontent-container'></div>
+
+        
+        </>
     );
 }
 
